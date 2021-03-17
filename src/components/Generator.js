1aba3370182803bdd407e2eff9ac120ce7542eaa
karolis3722 <karolis.siaulys94@gmail.com>
2021-03-17 18:18:06 +0200

    Update Generator.js

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -88,13 +88,13 @@ class Generator extends React.Component {
                     </Row>
                     <Row>
                         <Col xs={4}>
-                            <Button onClick={this.like} className='like-btn' size='lg'>Like</Button>
-                        </Col>
-                        <Col xs={4}>
                             <Button onClick={this.getPreviousMeme} className={this.state.haveLike ? '' : 'hide'} size='lg'>Liked one</Button>
                         </Col>
                         <Col xs={4}>
                             <Button onClick={this.getMeme} size='lg'>Generate</Button>
+                        </Col>
+                        <Col xs={4}>
+                            <Button onClick={this.like} className='like-btn' size='lg'>Like</Button>
                         </Col>
                     </Row>
                 </Container>
